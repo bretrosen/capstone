70c9d9711ce4b927ce7cbba61baaa20a83056f42
Bret Rosen <bretrosen@gmail.com>
2023-06-13 14:59:25 -0500

    modified user table and added prof and review table

---
+++ app/models/prof.py
@@ -0,0 +1,17 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+
+class Prof(db.Model):
+    __tablename__ = "profs"
+
+    if environment == "production":
+        __table_args__ = {"schema": SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    creator_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(20), nullable=False)
+    field = db.Column(db.String(50), nullable=False)
+
+    reviews = db.relationship('Review', back_populates='prof')
+    user = db.relationship('User', back_populates='profs')
---
+++ app/models/review.py
@@ -0,0 +1,43 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+
+class Review(db.Model):
+    __tablename__ = "reviews"
+
+    if environment == "production":
+        __table_args__ = {"schema": SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    creator_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    prof_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('profs.id')), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    intelligence = db.Column(db.Integer, nullable=False)
+    wisdom = db.Column(db.Integer, nullable=False)
+    charisma = db.Column(db.Integer, nullable=False)
+    knowledge = db.Column(db.Integer, nullable=False)
+    preparation = db.Column(db.Integer, nullable=False)
+    respect = db.Column(db.Integer, nullable=False)
+    for_credit = db.Column(db.Boolean, nullable=False)
+    attendance = db.Column(db.Boolean, nullable=False)
+    would_recommend = db.Column(db.Boolean, nullable=False)
+    textbook = db.Column(db.Boolean, nullable=False)
+
+    user = db.relationship('User', back_populates='reviews')
+    prof = db.relationship('Prof', back_populates='reviews')
+    course = db.relationship('Course', back_populates='reviews')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'creator_id': self.creator_id,
+            'prof_id': self.prof_id,
+            'course_id': self.course_id,
+            'intelligence': self.intelligence,
+            'wisdom': self.wisdom,
+            'knowledge': self.knowledge,
+            'respect': self.respect,
+            'for_credit': self.for_credit,
+            'attendance': self.attendance,
+            'would_recommend': self.would_recommend,
+            'textbook': self.textbook
+        }
