e263ab52d4f40f456712687b109bdc3b37838934
Bret Rosen <bretrosen@gmail.com>
2023-06-14 15:11:58 -0500

    post review route working jankily

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
 from sqlalchemy import desc
+from datetime import datetime
 from app.models import db, Review, Prof, Course
 from app.forms.post_review_form import PostReviewForm
 
@@ -13,7 +14,7 @@ def all_reviews():
     Query for all reviews for the display all review page. Order descending by datetime, so newest reviews appear first. Return results in a dictionary.
     '''
 
-    all_review_query = Review.query.all()
+    print("current user", current_user)
 
     all_reviews = Review.query.filter().order_by(desc(Review.time_stamp))
     return {'reviews': [review.to_dict() for review in all_reviews]}
@@ -35,11 +36,14 @@ def post_review():
     Query for current user, convert professor and course strings to the appropriate ids, add the review, and update the database.
     '''
 
-    user = current_user
-    creator_id = user.id
+    print("current user", current_user)
+
+    # user = current_user
+    # creator_id = user.id
+    creator_id = 1
     form = PostReviewForm()
 
-    # need to refine this, get it working for grabbing the first last name
+    # refactor this prob for more refined than last name
     form_prof_last_name = form.data['prof']
     db_prof = Prof.query.filter(Prof.last_name == form_prof_last_name).first()
     prof_id = db_prof.id
@@ -48,10 +52,30 @@ def post_review():
     db_course = Course.query.filter(Course.name == form_course_name).first()
     course_id = db_course.id
 
+    form['csrf_token'].data = request.cookies['csrf_token'] # Boilerplate code
+
     if form.validate_on_submit():
         new_review = Review(
             creator_id=creator_id,
             prof_id=prof_id,
             course_id=course_id,
-            review=form.data['review']
+            review=form.data['review'],
+            intelligence=form.data['intelligence'],
+            wisdom=form.data['wisdom'],
+            charisma=form.data['charisma'],
+            knowledge=form.data['knowledge'],
+            preparation=form.data['preparation'],
+            respect=form.data['respect'],
+            for_credit=form.data['for_credit'],
+            attendance=form.data['attendance'],
+            would_recommend=form.data['would_recommend'],
+            textbook=form.data['textbook'],
+            time_stamp=datetime.now()
         )
+
+        db.session.add(new_review)
+        db.session.commit()
+        return new_review.to_dict()
+
+    else:
+        return form.errors
--- app/forms/post_review_form.py
+++ app/forms/post_review_form.py
@@ -3,11 +3,14 @@ from wtforms import StringField, IntegerField, SelectField, BooleanField
 from wtforms.validators import DataRequired, ValidationError, Length, NumberRange
 from app.models import Prof, Course
 
-all_profs = Prof.query.all()
-PROFS = [prof.last_name for prof in all_profs]
-all_courses = Course.query.all
-COURSES = [course.name for course in all_courses]
+# all_profs = Prof.query.all()
+# PROFS = [prof.last_name for prof in all_profs]
+# all_courses = Course.query.all
+# COURSES = [course.name for course in all_courses]
 
+# FIX THIS
+PROFS = ['Shakespeare', 'Marx', 'Newton']
+COURSES = ['Market Economies', 'Anthropology of the Unknown', 'Postmodern American Literature']
 
 class PostReviewForm(FlaskForm):
     prof = SelectField("Prof Last Name", choices=PROFS)
