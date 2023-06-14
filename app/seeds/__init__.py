5f998dfbd9d0866e8fbb2b1d884570df14a8417d
Bret Rosen <bretrosen@gmail.com>
2023-06-14 07:13:18 -0500

    modified create table and seed order for postgres

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -23,33 +23,36 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_debate_topics()
+
         undo_debates()
-        undo_departments()
-        undo_courses()
-        undo_prof_courses()
-        undo_profs()
         undo_reviews()
+        undo_prof_courses()
+        undo_courses()
+        undo_profs()
+        undo_debate_topics()
+        undo_departments()
         undo_users()
     seed_users()
-    seed_reviews()
+    seed_departments()
+    seed_debate_topics()
     profs = seed_profs()
     seed_courses(profs)
-    seed_departments()
+    seed_reviews()
     seed_debates()
-    seed_debate_topics()
+
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_debate_topics()
+
     undo_debates()
-    undo_departments()
-    undo_courses()
-    undo_prof_courses()
-    undo_profs()
     undo_reviews()
+    undo_prof_courses()
+    undo_courses()
+    undo_profs()
+    undo_debate_topics()
+    undo_departments()
     undo_users()
     # Add other undo functions here
