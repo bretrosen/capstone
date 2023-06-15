e16d58ef82e60d8b700df44b4b47b3aee5fce6cf
Bret Rosen <bretrosen@gmail.com>
2023-06-15 09:11:17 -0500

    single review page up

--- react-app/src/components/SingleReview/index.js
+++ react-app/src/components/SingleReview/index.js
@@ -20,6 +20,41 @@ export const SingleReview = () => {
     }
 
     return (
-        <h1>Single Review Page...</h1>
+        <div className='review-wrapper'>
+            <div className='review-attributes'>
+                <div>Intelligence: {review.intelligence}</div>
+                <div>Wisdom: {review.wisdom}</div>
+                <div>Charisma: {review.charisma}</div>
+                <div>Knowledge: {review.knowledge}</div>
+                <div>Preparation: {review.preparation}</div>
+                <div>Respect: {review.respect}</div>
+            </div>
+            <div className='review-summary'>
+                <div>
+                    <div>Course: {review.course_id}</div>
+                    <div>Prof: {review.prof_id}</div>
+                    <div>{review.time_stamp}</div>
+                </div>
+                <div>
+                    <div>For Credit:
+                        {review.for_credit && `Yes`}
+                        {!review.for_credit && `No`}
+                    </div>
+                    <div>Attendance:
+                        {review.attendance && `Mandatory`}
+                        {!review.attendance && `Optional`}
+                    </div>
+                    <div>Would Take Again:
+                        {review.would_recommend && `Yes`}
+                        {!review.would_recommend && `No`}
+                    </div>
+                    <div>Textbook:
+                        {review.textbook && `Yes`}
+                        {!review.textbook && `No`}
+                    </div>
+                </div>
+            </div>
+            <div className='review-text'>{review.review}</div>
+        </div>
     )
 }
