94a7b2286d3490ad168ac2f1fd70c32f850aa2c9
Bret Rosen <bretrosen@gmail.com>
2023-06-17 06:56:01 -0500

    styled all reviews

--- react-app/src/components/Reviews/index.js
+++ react-app/src/components/Reviews/index.js
@@ -25,44 +25,54 @@ export const ReviewList = () => {
         return <h1>Loading...</h1>
     }
 
+    // classNames for variable colors in review data
+
+
     return (
         <div className='reviews-list-wrapper'>
             {reviews.map((review) => (
                 <>
-                <Link to={`/reviews/${review.id}`} key={review.id}>
-                    <div className='reviews-list-item' >
+                    <Link to={`/reviews/${review.id}`} key={review.id}>
+                        <div className='reviews-list-item' >
 
-                        <div className='reviews-list-ratings'>
-                            <div>Quality: {review.quality.toFixed(1)}</div>
-                            <div>Difficulty: {review.difficulty}</div>
-                        </div>
-                        <div className='reviews-list-right'>
-                            <div className='reviews-right-top'>
-                                <div className='review-item'>{review.course_name}</div>
-                                <div className='review-item'>Prof: {review.prof_first_name} {review.prof_last_name}</div>
-                                <div>{review.time_stamp}</div>
+                            <div className='reviews-list-ratings'>
+                                <div className='rating-heading'>Quality</div>
+                                {review.quality > 0 && review.quality < 6.7 &&
+                                    <div className='rating-number' id='low'>{review.quality.toFixed(1)}</div>}
+                                {review.quality >= 6.7 && review.quality < 13.4 &&
+                                    <div className='rating-number' id='medium'>{review.quality.toFixed(1)}</div>}
+                                {review.quality >= 13.4 && review.quality <= 20 &&
+                                    <div className='rating-number' id='high'>{review.quality.toFixed(1)}</div>}
+                                <div className='rating-heading'>Difficulty</div>
+                                <div className='rating-number-difficulty'>{review.difficulty.toFixed(1)}</div>
                             </div>
-                            <div className='reviews-right-middle'>
-                                <div className='review-item'>For Credit:
-                                    {review.for_credit && `Yes`}
-                                    {!review.for_credit && `No`}
+                            <div className='reviews-list-right'>
+                                <div className='reviews-right-top'>
+                                    <div className='review-course-name'>{review.course_name}</div>
+                                    <div className='review-prof-name'>Professor: {review.prof_first_name} {review.prof_last_name}</div>
+                                    <div className='review-time'>{review.time_stamp}</div>
                                 </div>
-                                <div className='review-item'>Attendance:
-                                    {review.attendance && `Mandatory`}
-                                    {!review.attendance && `Optional`}
+                                <div className='reviews-right-middle'>
+                                    <div className='review-item'>For Credit:&nbsp;
+                                        {review.for_credit && `Yes`}
+                                        {!review.for_credit && `No`}
+                                    </div>
+                                    <div className='review-item'>Attendance:&nbsp;
+                                        {review.attendance && `Mandatory`}
+                                        {!review.attendance && `Optional`}
+                                    </div>
+                                    <div className='review-item'>Would Take Again:&nbsp;
+                                        {review.would_recommend && `Yes`}
+                                        {!review.would_recommend && `No`}
+                                    </div>
+                                    <div className='review-item'>Textbook:&nbsp;
+                                        {review.textbook && `Yes`}
+                                        {!review.textbook && `No`}
+                                    </div>
                                 </div>
-                                <div className='review-item'>Would Take Again:
-                                    {review.would_recommend && `Yes`}
-                                    {!review.would_recommend && `No`}
-                                </div>
-                                <div className='review-item'>Textbook:
-                                    {review.textbook && `Yes`}
-                                    {!review.textbook && `No`}
-                                </div>
+                                <div className='reviews-right-bottom'>{review.review}</div>
                             </div>
-                            <div className='reviews-right-bottom'>{review.review}</div>
                         </div>
-                    </div>
 
                     </Link>
 
