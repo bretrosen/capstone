a9e54176f04945fcb4f00e411f64fc9ae81533b9
Bret Rosen <bretrosen@gmail.com>
2023-06-18 08:20:16 -0500

    starting prof routes

--- app/api/prof_routes.py
+++ app/api/prof_routes.py
@@ -9,8 +9,10 @@ prof_routes = Blueprint('profs', __name__)
 @prof_routes.route('/')
 def all_profs():
     '''
-    Query for all profs. Return results in a dictionary.
+    First, query for all reviews, and get the average rating score for each prof. Query for all profs for the display all profs page. Return results in a dictionary.
     '''
+    all_reviews = Review.query.all()
+    reviews_dict = [review.to_dict() for review in all_reviews]
 
     all_profs = Prof.query.all()
     profs_dict = [prof.to_dict() for prof in all_profs]
