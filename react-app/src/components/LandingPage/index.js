abb87d410ea24599782bab0e5780937796105636
Bret Rosen <bretrosen@gmail.com>
2023-06-20 09:33:21 -0500

    lil more work on login

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -22,7 +22,7 @@ export const LandingPage = () => {
         <div className='landing-page'>
             <div className='get-started'>
                 {/* <img className='man-book' src='/static/man-reading-book.jpeg' alt='man reading book'></img> */}
-                    <div className='enter'>
+                    {/* <div className='enter'>
                         <NavLink to='/reviews'>All Reviews</NavLink>
                         <br></br>
                         <NavLink to='/reviews/new'>New Review</NavLink>
@@ -30,7 +30,7 @@ export const LandingPage = () => {
                         <NavLink to='/profs'>All Professors</NavLink>
                         <br></br>
                         <NavLink to='/profs/new'>New Professor</NavLink>
-                    </div>
+                    </div> */}
 
 
             </div>
--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -5,31 +5,40 @@ import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
-	const sessionUser = useSelector(state => state.session.user)
+    const sessionUser = useSelector(state => state.session.user)
 
-	return (
-		<>
-        <div className='nav-wrapper'>
-            <NavLink className='nav-logo' to='/'>
-                Home
-            </NavLink>
-            <div className='nav-menu-center'>
-            </div>
-            <div className='nav-wrapper-right'>
+    return (
+        <>
+            <div className='nav-wrapper'>
+                <NavLink className='nav-logo' to='/'>
+                    Home
+                </NavLink>
+                <div className='nav-menu-center'>
+                    <NavLink to='/reviews' className='nav-link'>All Reviews</NavLink>
+                    <NavLink to='/profs' className='nav-link'>All Professors</NavLink>
+                    {sessionUser &&
+                        <>
+                            <NavLink to='/reviews/new' className='nav-link'>New Review</NavLink>
+                            <NavLink to='/profs/new' className='nav-link'>New Professor</NavLink>
+                        </>
+                    }
+
+                </div>
+                <div className='nav-wrapper-right'>
 
                     {isLoaded && (
-                        
-                            <ProfileButton user={sessionUser} />
+
+                        <ProfileButton user={sessionUser} />
 
                     )}
 
+                </div>
             </div>
-        </div>
 
-		</>
+        </>
 
 
-	);
+    );
 }
 
 export default Navigation;
