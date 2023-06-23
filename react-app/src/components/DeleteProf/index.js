bca9c4c973f0d6cdd69623c8104660a0891e69ec
Bret Rosen <bretrosen@gmail.com>
2023-06-23 11:12:55 -0500

    all reviews updating with prof delete

--- react-app/src/components/DeleteProf/index.js
+++ react-app/src/components/DeleteProf/index.js
@@ -1,22 +1,34 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useModal } from "../../context/Modal"
 import { deleteProfThunk } from '../../store/profs'
+import { deleteReviewThunk } from '../../store/reviews'
+import './DeleteProf.css'
 
 const DeleteProf = ({ profId }) => {
     const dispatch = useDispatch()
     const { closeModal } = useModal()
 
-    const handleDelete = (event) => {
-        return dispatch(deleteProfThunk(profId))
+    const reviewsObj = useSelector(state => state.reviews.allReviews)
+    const reviews = Object.values(reviewsObj)
+
+
+    const handleDelete = () => {
+        reviews.forEach(review => {
+            if (review.prof_id === profId) {
+                dispatch(deleteReviewThunk(review.id))
+            }
+        })
+
+        dispatch(deleteProfThunk(profId))
             .then(closeModal)
     }
 
     return (
-        <div>
+        <div className='delete-prof-wrapper'>
             <h2>Confirm Delete</h2>
             <p>Are you sure you want to remove this professor?</p>
-            <button className='regular-button' onClick={handleDelete}>Yes (Delete Professor)</button>
+            <button className='regular-button' onClick={handleDelete}>Yes (Delete Professor)</button>&nbsp;&nbsp;
             <button className='regular-button' onClick={closeModal}>No (Keep Professor)</button>
         </div>
     )
--- react-app/src/components/Reviews/index.js
+++ react-app/src/components/Reviews/index.js
@@ -16,15 +16,21 @@ export const ReviewList = () => {
     const dispatch = useDispatch()
     const history = useHistory()
 
+
+
+    useEffect( () => {
+        const fetchReviews = async () => {
+            dispatch(getAllReviewsThunk())
+            console.log("useEffect in get all reviews ran")
+        }
+        fetchReviews()
+    }, [dispatch])
+
     const reviewsObj = useSelector(state => state.reviews.allReviews)
     const reviews = Object.values(reviewsObj)
     const user = useSelector(state => state.session.user)
+    console.log("reviews array in get all reviews page ==========>", reviews)
     console.log("review length in get all reviews page ==========>", reviews.length)
-
-    useEffect(() => {
-        dispatch(getAllReviewsThunk())
-        console.log("useEffect in get all reviews ran")
-    }, [dispatch])
 
     if (!reviews) {
         return <h1>Loading...</h1>
