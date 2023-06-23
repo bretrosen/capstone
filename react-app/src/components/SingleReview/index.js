7d4fbce81775c4efe8bc83957e06c7157d094d62
Bret Rosen <bretrosen@gmail.com>
2023-06-23 13:00:45 -0500

    single review styled like performance review

--- react-app/src/components/SingleReview/index.js
+++ react-app/src/components/SingleReview/index.js
@@ -25,7 +25,118 @@ export const SingleReview = () => {
 
     return (
         <div className='single-review-wrapper'>
-            <div className='single-review-heading'>Review of {review.course_name} as taught by Professor {review.prof_first_name} {review.prof_last_name}</div>
+            <h1 className='single-review-h1'>Professor Performance Review</h1>
+
+
+            <div classname='review-top'>
+                <h2 className='single-review-h2'>Professor and Course Information</h2>
+                <div className='info-box'>
+
+
+                    <div className='info-left'>
+                        <div className='info-row'>
+                            <div className='info-heading'>Professor Name</div>
+                            <div className='info-value'>{review.prof_first_name} {review.prof_last_name}</div>
+                        </div>
+                        <div className='info-row'>
+                            <div className='info-heading'>Professor ID</div>
+                            <div className='info-value'>{review.prof_id} </div>
+                        </div>
+                        <div className='info-row'>
+                            <div className='info-heading'>Course Name</div>
+                            <div className='info-value'>{review.course_name} </div>
+                        </div>
+                        <div className='info-row'>
+                            <div className='info-heading'>Course ID</div>
+                            <div className='info-value'>{review.course_id} </div>
+                        </div>
+                    </div>
+
+                    <div className='info-right'>
+                        <div className='info-row'>
+                            <div className='boolean-heading'>Course Taken For Credit</div>
+                            <div className='boolean-value'>
+                                {review.for_credit && `Yes`}
+                                {!review.for_credit && `No`}
+                            </div>
+                        </div>
+                        <div className='info-row'>
+                            <div className='boolean-heading'>Attendance</div>
+                            <div className='boolean-value'>
+                                {review.attendance && `Mandatory`}
+                                {!review.attendance && `Optional`}
+                            </div>
+                        </div>
+                        <div className='info-row'>
+                            <div className='boolean-heading'>Would Take Again</div>
+                            <div className='boolean-value'>
+                                {review.would_recommend && `Yes`}
+                                {!review.would_recommend && `No`}
+                            </div>
+                        </div>
+                        <div className='info-row'>
+                            <div className='boolean-heading'>Textbook</div>
+                            <div className='boolean-value'>
+                                {review.textbook && `Yes`}
+                                {!review.textbook && `No`}
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className='review-bottom'>
+                <div className='bottom-left'>
+                    <h2 className='single-review-h2'>Professor Attributes</h2>
+                    <div className='info-row'>
+                        <div className='info-heading'>Intelligence</div>
+                        <div className='num-value'>{review.intelligence}</div>
+                    </div>
+                    <div className='info-row'>
+                        <div className='info-heading'>Wisdom</div>
+                        <div className='num-value'>{review.wisdom}</div>
+                    </div>
+                    <div className='info-row'>
+                        <div className='info-heading'>Charisma</div>
+                        <div className='num-value'>{review.charisma}</div>
+                    </div>
+                    <div className='info-row'>
+                        <div className='info-heading'>Knowledge</div>
+                        <div className='num-value'>{review.knowledge}</div>
+                    </div>
+                    <div className='info-row'>
+                        <div className='info-heading'>Preparation</div>
+                        <div className='num-value'>{review.preparation}</div>
+                    </div>
+                    <div className='info-row'>
+                        <div className='info-heading'>Respect</div>
+                        <div className='num-value'>{review.respect}</div>
+                    </div>
+                    <div className='info-row'>
+                        <div className='info-heading'>Difficulty</div>
+                        <div className='num-value'>{review.difficulty}</div>
+                    </div>
+                    <div className='info-row'>
+                        <div className='info-heading'>Overall Quality</div>
+                        <div className='num-value'>{review.quality.toFixed(1)}</div>
+                    </div>
+                </div>
+
+                <div className='bottom-right'>
+                    <h2 className='single-review-h2-comments'>Reviewer Comments</h2>
+                    <div className='review-text'>{review.review}</div>
+                    <div className='review-date'>Review Date: {dayjs(review.time_stamp).format("MMMM Do, YYYY")}</div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+            {/* <div className='single-review-heading'>Review of {review.course_name} as taught by Professor {review.prof_first_name} {review.prof_last_name}</div>
             <div className='single-review-container'>
                 <div className='single-review-summary'>
 
@@ -57,7 +168,7 @@ export const SingleReview = () => {
                     <div>Respect: {review.respect}</div>
                     <div>Difficulty: {review.difficulty}</div>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
