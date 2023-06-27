9686f7de1263993ffc5b139029e6cb1bfa37fb16
Bret Rosen <bretrosen@gmail.com>
2023-06-27 12:11:05 -0500

    scroll to top for single course

--- react-app/src/components/SingleCourse/index.js
+++ react-app/src/components/SingleCourse/index.js
@@ -24,7 +24,7 @@ export const SingleCourse = () => {
 
     let courseIdToKeyInto
     // find the right index in the courses array to access
-    for (let i = 0;  i <= courses.length; i++) {
+    for (let i = 0; i <= courses.length; i++) {
         if (courses[i]?.id == courseId) {
             courseIdToKeyInto = i
         }
@@ -34,6 +34,9 @@ export const SingleCourse = () => {
 
     const reviewsObj = useSelector(state => state.reviews.allReviews)
     const reviews = Object.values(reviewsObj)
+
+    // ensure we start at the top of the screen
+    window.scrollTo(0, 0)
 
     useEffect(() => {
         dispatch(getSingleCourseThunk(courseId))
@@ -47,103 +50,103 @@ export const SingleCourse = () => {
 
     return (
         <div className='single-prof-wrapper'>
-        <div className='single-prof-top'>
-            <div className='single-prof-top-left'>
-                <div className='single-prof-quality'>
-                    <div className='single-prof-quality-number'>
-                        {courses[courseIdToKeyInto]?.quality?.toFixed(1)}
-                    </div>
-                    <div className='quality-out-of'>
-                        / 20
-                    </div>
+            <div className='single-prof-top'>
+                <div className='single-prof-top-left'>
+                    <div className='single-prof-quality'>
+                        <div className='single-prof-quality-number'>
+                            {courses[courseIdToKeyInto]?.quality?.toFixed(1)}
+                        </div>
+                        <div className='quality-out-of'>
+                            / 20
+                        </div>
                     </div>
                     <div className='quality-lower'>
-                    <div className='quality-label'>
-                        Overall Quality based on {course.reviews.length} ratings
-                    </div>
-                    <div className='single-prof-name'>
-                        {course.name}
-                    </div>
-                    <div className='single-prof-department'>
-                        Course in the {course.department} department
-                    </div>
-                    <div className='single-prof-aggregates'>
-                        <div className='single-prof-recommends'>
-                            <div className='recommends-number'>
-                                {courses[courseIdToKeyInto]?.recommended?.toFixed(1)} %
+                        <div className='quality-label'>
+                            Overall Quality based on {course.reviews.length} ratings
+                        </div>
+                        <div className='single-prof-name'>
+                            {course.name}
+                        </div>
+                        <div className='single-prof-department'>
+                            Course in the {course.department} department
+                        </div>
+                        <div className='single-prof-aggregates'>
+                            <div className='single-prof-recommends'>
+                                <div className='recommends-number'>
+                                    {courses[courseIdToKeyInto]?.recommended?.toFixed(1)} %
+                                </div>
+                                <div className='recommends-text'>
+                                    Would take again
+                                </div>
                             </div>
-                            <div className='recommends-text'>
-                                Would take again
+                            <div className='single-prof-difficulty'>
+                                <div className='difficulty-number'>
+                                    {courses[courseIdToKeyInto]?.difficulty?.toFixed(1)}
+                                </div>
+                                <div className='difficulty-text'>
+                                    Level of difficulty
+                                </div>
                             </div>
                         </div>
-                        <div className='single-prof-difficulty'>
-                            <div className='difficulty-number'>
-                                {courses[courseIdToKeyInto]?.difficulty?.toFixed(1)}
-                            </div>
-                            <div className='difficulty-text'>
-                                Level of difficulty
-                            </div>
-                        </div>
                     </div>
+                    {/* <button className='regular-button' onClick={() => history.push(`/reviews/new`)}>Rate Professor {prof.last_name}</button> */}
                 </div>
-                {/* <button className='regular-button' onClick={() => history.push(`/reviews/new`)}>Rate Professor {prof.last_name}</button> */}
-            </div>
-            <div className='bar-chart'>
-                <div className='chart-heading'>
-                    Rating Distribution
-                </div>
+                <div className='bar-chart'>
+                    <div className='chart-heading'>
+                        Rating Distribution
+                    </div>
                     <RatingDistribution />
+                </div>
             </div>
-        </div>
-        <div className='single-prof-reviews'>
+            <div className='single-prof-reviews'>
 
-        {courseReviews.map((review) => (
-            <>
-                <Link to={`/reviews/${review.id}`} key={review.id}>
-                    <div className='reviews-list-item' >
+                {courseReviews.map((review) => (
+                    <>
+                        <Link to={`/reviews/${review.id}`} key={review.id}>
+                            <div className='reviews-list-item' >
 
-                        <div className='reviews-list-ratings'>
-                            <div className='rating-heading'>Quality</div>
-                            {review.quality > 0 && review.quality < 6.7 &&
-                                <div className='rating-number' id='low'>{review.quality.toFixed(1)}</div>}
-                            {review.quality >= 6.7 && review.quality < 13.4 &&
-                                <div className='rating-number' id='medium'>{review.quality.toFixed(1)}</div>}
-                            {review.quality >= 13.4 && review.quality <= 20 &&
-                                <div className='rating-number' id='high'>{review.quality.toFixed(1)}</div>}
-                            <div className='rating-heading'>Difficulty</div>
-                            <div className='rating-number-difficulty'>{review.difficulty.toFixed(1)}</div>
-                        </div>
-                        <div className='reviews-list-right'>
-                            <div className='reviews-right-top'>
-                                <div className='review-course-name'>Professor {review.prof_name}</div>
+                                <div className='reviews-list-ratings'>
+                                    <div className='rating-heading'>Quality</div>
+                                    {review.quality > 0 && review.quality < 6.7 &&
+                                        <div className='rating-number' id='low'>{review.quality.toFixed(1)}</div>}
+                                    {review.quality >= 6.7 && review.quality < 13.4 &&
+                                        <div className='rating-number' id='medium'>{review.quality.toFixed(1)}</div>}
+                                    {review.quality >= 13.4 && review.quality <= 20 &&
+                                        <div className='rating-number' id='high'>{review.quality.toFixed(1)}</div>}
+                                    <div className='rating-heading'>Difficulty</div>
+                                    <div className='rating-number-difficulty'>{review.difficulty.toFixed(1)}</div>
+                                </div>
+                                <div className='reviews-list-right'>
+                                    <div className='reviews-right-top'>
+                                        <div className='review-course-name'>Professor {review.prof_name}</div>
 
-                                <div className='review-time'>{dayjs(review.time_stamp).format("MMMM Do, YYYY")}</div>
+                                        <div className='review-time'>{dayjs(review.time_stamp).format("MMMM Do, YYYY")}</div>
+                                    </div>
+                                    <div className='reviews-right-middle'>
+                                        <div className='review-item'>For Credit:&nbsp;
+                                            {review.for_credit && <span className='review-boolean'>Yes</span>}
+                                            {!review.for_credit && <span className='review-boolean'>No</span>}
+                                        </div>
+                                        <div className='review-item'>Attendance:&nbsp;
+                                            {review.attendance && <span className='review-boolean'>Mandatory</span>}
+                                            {!review.attendance && <span className='review-boolean'>Optional</span>}
+                                        </div>
+                                        <div className='review-item'>Would Take Again:&nbsp;
+                                            {review.would_recommend && <span className='review-boolean'>Yes</span>}
+                                            {!review.would_recommend && <span className='review-boolean'>No</span>}
+                                        </div>
+                                        <div className='review-item'>Textbook:&nbsp;
+                                            {review.textbook && <span className='review-boolean'>Yes</span>}
+                                            {!review.textbook && <span className='review-boolean'>No</span>}
+                                        </div>
+                                    </div>
+                                    <div className='reviews-right-bottom'>{review.review}</div>
+                                </div>
                             </div>
-                            <div className='reviews-right-middle'>
-                                <div className='review-item'>For Credit:&nbsp;
-                                    {review.for_credit && <span className='review-boolean'>Yes</span>}
-                                    {!review.for_credit && <span className='review-boolean'>No</span>}
-                                </div>
-                                <div className='review-item'>Attendance:&nbsp;
-                                    {review.attendance && <span className='review-boolean'>Mandatory</span>}
-                                    {!review.attendance && <span className='review-boolean'>Optional</span>}
-                                </div>
-                                <div className='review-item'>Would Take Again:&nbsp;
-                                    {review.would_recommend && <span className='review-boolean'>Yes</span>}
-                                    {!review.would_recommend && <span className='review-boolean'>No</span>}
-                                </div>
-                                <div className='review-item'>Textbook:&nbsp;
-                                    {review.textbook && <span className='review-boolean'>Yes</span>}
-                                    {!review.textbook && <span className='review-boolean'>No</span>}
-                                </div>
-                            </div>
-                            <div className='reviews-right-bottom'>{review.review}</div>
-                        </div>
-                    </div>
 
-                </Link>
+                        </Link>
 
-                {/* {user && review.creator_id === user.id &&
+                        {/* {user && review.creator_id === user.id &&
                     <button className='regular-button' onClick={() => history.push(`/reviews/${review.id}/edit`)}>Update Rating</button>}
                 &nbsp;&nbsp;
                 {user && review.creator_id === user.id &&
@@ -154,13 +157,13 @@ export const SingleCourse = () => {
                     />
                 } */}
 
-            </>
+                    </>
 
-        ))}
+                ))}
 
 
 
+            </div>
         </div>
-    </div>
     )
 }
